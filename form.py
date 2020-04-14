271087cbf4e241c6093455860f820979c14b6364
Ubuntu <ubuntu@ip-172-31-84-29.ec2.internal>
2020-04-14 07:24:58 +0000

    T

--- form.py
+++ form.py
@@ -22,3 +22,11 @@ class ProfileForm(FlaskForm):
     picture = FileField('Profile Picture', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg', 'PNG'], 'Images only!')],id="profilepicture")
     "2020-05-14T13:15:29+05:30"
 kndioesokodsknfkdkn
+ firstname = StringField('Firstname', validators=[DataRequired()], id="fname")
+    lastname = StringField('Lastname', validators=[DataRequired()], id="lname")
+    gender = SelectField('Gender',  choices=[('S','Select Gender'), ('Female', 'Female'), ('Male', 'Male')], id="gender")
+    email = StringField('Email',[validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")], render_kw={"placeholder": "e.g. jdoe@example.com "}, id="email")
+    location = StringField ('Location', validators=[DataRequired()], render_kw={"placeholder": "e.g. Kingston,Jamaica "}, id="location")
+    biograpy = TextAreaField ('Biography', validators=[DataRequired()], id="bio")
+    picture = FileField('Profile Picture', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg', 'PNG'], 'Images only!')],id="profilepicture")
+    "2020-05-14T13:15:29+05:30"
