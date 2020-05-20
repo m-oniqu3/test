4e7faad1b044f496ddfd3091700c0b8168258adc
Ubuntu <ubuntu@ip-172-31-84-29.ec2.internal>
2020-05-20 07:00:37 +0000

    Two

--- form.py
+++ form.py
@@ -12,3 +12,11 @@ class ProfileForm(FlaskForm):
     location = StringField ('Location', validators=[DataRequired()], render_kw={"placeholder": "e.g. Kingston,Jamaica "}, id="location")
     biograpy = TextAreaField ('Biography', validators=[DataRequired()], id="bio")
     picture = FileField('Profile Picture', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg', 'PNG'], 'Images only!')],id="profilepicture")
+
+    firstname = StringField('Firstname', validators=[DataRequired()], id="fname")
+    lastname = StringField('Lastname', validators=[DataRequired()], id="lname")
+    gender = SelectField('Gender',  choices=[('S','Select Gender'), ('Female', 'Female'), ('Male', 'Male')], id="gender")
+    email = StringField('Email',[validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")], render_kw={"placeholder": "e.g. jdoe@example.com "}, id="email")
+    location = StringField ('Location', validators=[DataRequired()], render_kw={"placeholder": "e.g. Kingston,Jamaica "}, id="location")
+    biograpy = TextAreaField ('Biography', validators=[DataRequired()], id="bio")
+    picture = FileField('Profile Picture', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg', 'PNG'], 'Images only!')],id="profilepicture")
